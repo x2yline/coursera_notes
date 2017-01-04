26851cf7013440c208737d64cfe4be6d4560a4a6
a medical student <x2yline@users.noreply.github.com>
2017-01-04 12:41:10 +0800

    Create week1-3 Sequences of Numbers.R

---
+++ 1.programming with R/week1-3 Sequences of Numbers.R
@@ -0,0 +1,11 @@
+1:20
+pi:10
+15:1
+?':'
+seq(1, 20)
+seq(0, 10, by=0.5)
+my_seq <- seq(5,10, length=30)
+seq(along.with = my_seq)
+seq_along(my_seq)
+rep(c(0, 1, 2), times = 10)
+rep(c(0, 1, 2), each = 10)

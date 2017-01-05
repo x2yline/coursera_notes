a6b4966fe133d65656b27d1db45be90d6528c81d
a medical student <x2yline@users.noreply.github.com>
2017-01-05 21:01:56 +0800

    Create 7 Dates and Times.R

---
+++ 1.programming with R/week2/7 Dates and Times.R
@@ -0,0 +1,29 @@
+d1 <- Sys.Date()
+class(d1)
+unclass(d1) # the exact number of days since 1970-01-01!
+d1
+
+d2 <- as.Date('1969-01-01')
+unclass(d2)
+
+t1<- Sys.time()
+t1
+class(t1)
+unclass(t1)
+t2 <- as.POSIXlt(Sys.time())
+class(t2)
+t2
+unclass(t2)
+str(unclass(t2))
+t2$min
+weekdays(d1)
+months(t1)
+quarters(t2)
+
+t3 <- 'October 17, 1986 08:24'
+t4 <- strptime(t3, '%B %d, %Y %H:%M')
+t4
+class(t4)
+Sys.time() > t1
+Sys.time() - t1
+difftime(Sys.time(), t1, units = 'days')

5e417b5d8e503acd32998a64a4f76dff3712d1bb
a medical student <x2yline@users.noreply.github.com>
2017-01-05 22:41:50 +0800

    Create homework_part2.R

---
+++ 1.programming with R/week2/homework_part2.R
@@ -0,0 +1,19 @@
+calculate_cases <- function(directory, single_id){
+  if (single_id <10){
+    single_id = paste('00' , as.character(single_id), sep='')
+  }else if (single_id <100){
+    single_id = paste('0' , as.character(single_id), sep='')
+  }else {
+    single_id = as.character(single_id)
+  }
+  df <- read.csv(file.path(directory, paste(single_id,'.csv',sep='')))
+  sum_num <- length(df[1][!is.na(df[2]) & !is.na(df[3])])
+}
+
+complete <- function(directory, id=1:332){
+  nobs = NULL
+  for (i in id) {
+    nobs <- c(nobs,calculate_cases(directory, i))
+  }
+  df <- data.frame(id, nobs)
+}

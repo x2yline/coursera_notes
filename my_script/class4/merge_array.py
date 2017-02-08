e9a061188f6f703af914430fcdaff5a64dfd513b
a medical student <x2yline@users.noreply.github.com>
2017-02-08 23:32:36 +0800

    Update merge_array.py

--- my_script/class4/merge_array.py
+++ my_script/class4/merge_array.py
@@ -1,22 +1,17 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
 
-This is a temporary script file.
-"""
 
-import tarfile
 import os
-import gzip
 from collections import OrderedDict
 
 def untar(fname, untar_path):
+    import tarfile
     if not untar_path:
         untar_path = ''.join(fname.split('.')[:-1])
     t = tarfile.open(fname, 'r')
     t.extractall(path = untar_path)
     t.close()
      
+    
     
     
 fname = r'G:\GSE48213_RAW.tar'
@@ -35,14 +30,23 @@ def merge_array(file_list, dirs = '.' ):
     counts = -1
     for i in file_list:
         counts += 1
-        
-        with gzip.open(os.path.join(dirs, i),'r') as f:
-            for line in f:
-                item = line.split('\t')
-                if item[0] in my_dict:
-                    my_dict[item[0]] += (counts - len(my_dict[item[0]]))*[''] + [item[1][:-1]]
-                else:
-                    my_dict[item[0]] = counts*[''] + [item[1][:-1]]
+        if i[-3:] == '.gz':
+            import gzip
+            with gzip.open(os.path.join(dirs, i),'r') as f:
+                for line in f:
+                    item = line.split('\t')
+                    if item[0] in my_dict:
+                        my_dict[item[0]] += (counts - len(my_dict[item[0]]))*[''] + [item[1][:-1]]
+                    else:
+                        my_dict[item[0]] = counts*[''] + [item[1][:-1]]
+        else:
+            with open(os.path.join(dirs, i),'r') as f:
+                for line in f:
+                    item = line.split('\t')
+                    if item[0] in my_dict:
+                        my_dict[item[0]] += (counts - len(my_dict[item[0]]))*[''] + [item[1][:-1]]
+                    else:
+                        my_dict[item[0]] = counts*[''] + [item[1][:-1]]
     return my_dict
 
 def write_dict_to_csv(my_dict, path = '.\\merged.csv'):
@@ -50,9 +54,8 @@ def write_dict_to_csv(my_dict, path = '.\\merged.csv'):
     for i in my_dict.keys():
         content += i + ','
         content += ','.join(my_dict[i]) + '\n'
-    with open(path,'w') as f:
+    with open(path,'a') as f:
         f.write(content)
-    return content
         
 my_dict = merge_array(file_list, dirs = untar_path)
-content = write_dict_to_csv(my_dict, path = 'G:\\1.csv')
+write_dict_to_csv(my_dict, path = 'G:\\1.csv')

e953b15e3eaeecf1bef00ab3cba5443d80a9473f
a medical student <x2yline@users.noreply.github.com>
2017-02-18 00:01:04 +0800

    Update exon_length.py

--- my_script/class5/exon_length.py
+++ my_script/class5/exon_length.py
@@ -29,14 +29,17 @@ def find_target_data(gene_name, gtf_file_path, chunk=3072*2048):
             raise NameError('your gene_name is not exit')
         print ("\nHave got the gene information!")
     return(target_data)
-
-def draw_gene_structure(gene_name,target_data, png_path='gene_structure.png'):
+ 
+def draw_gene_structure(gene_name,target_data, png_path=''):
+    gene_symbol = gene_name.upper()
+    if not png_path:
+        png_path = gene_symbol+'.png'
     #定义颜色的字典
-    tmp_colors = ['lime','red', 'blue', 'yellow','yellow']
-    names_tmp_colors = [ 'gene','CDS','exon','three_prime_utr','five_prime_utr']
-    colors_legend_name = ['gene','CDS_exon', 'non_CDS_exon', 'UTR_exon', 'UTR_exon']
+    tmp_colors = ['lime','red', 'blue', 'yellow','yellow','w']
+    names_tmp_colors = [ 'gene','CDS','exon','three_prime_utr','five_prime_utr','stop_codon']
+    colors_legend_name = ['gene','CDS_exon', 'non_CDS_exon', 'UTR_exon']
     color_dict = dict(zip(names_tmp_colors,tmp_colors))
-    
+     
     # 提取转录本名称
     import re
     transcript_list = []
@@ -52,13 +55,14 @@ def draw_gene_structure(gene_name,target_data, png_path='gene_structure.png'):
     for i in target_data['record']:
         if i =='transcript':
            transcript_num += 1
-           
+            
     import numpy as np
     import matplotlib.patches as mpatches
     import matplotlib.pyplot as plt
     import matplotlib.lines as lines
     fig = plt.figure(1)
-    
+    # 设置不透明度，默认为1
+    fig.patch.set_alpha(1)
     num = 0#当前转录本数目标志
     warnings = []
     for i in range(len(target_data['record'])):
@@ -69,13 +73,14 @@ def draw_gene_structure(gene_name,target_data, png_path='gene_structure.png'):
             else:
                 arr = '<-'
             #图的第一个区域
+            # add_axes 是在一张图上指定特定区域作图，第一个数字为从左边%20处，下面20%处开始，宽50%，高60%区域作图
             ax = fig.add_axes([0.2,0.2,0.5,0.6])
             # 定义基因方向箭头
             arrow= mpatches.FancyArrowPatch(
             (int(target_data['start'][i]), 0.1),
             (int(target_data['end'][i]), 0.1),
             arrowstyle= arr,
-            mutation_scale=25, lw=1, color='lime')
+            mutation_scale=25, lw=1, color='lime',antialiased=True)#antialiased默认为True，边缘平滑处理
             # 画箭头
             ax.add_patch(arrow)
             # 坐标轴标签
@@ -102,47 +107,59 @@ def draw_gene_structure(gene_name,target_data, png_path='gene_structure.png'):
             num += 1 # 转录本所有区域计数作图
             line1 = [(int(target_data['start'][i]),num), (int(target_data['end'][i]),num)]
             (line1_xs, line1_ys) = zip(*line1)
-            ax.add_line(lines.Line2D(line1_xs, line1_ys, linewidth=0.5, color='black'))
+            ax.add_line(lines.Line2D(line1_xs, line1_ys, linewidth=0.2,
+                                     solid_capstyle = 'butt',solid_joinstyle='miter',
+                                     antialiased=False,color='black'))
         elif target_data['record'][i] in color_dict.keys():
             # 添加结构图
-            line2 = [(int(target_data['start'][i]),num), (int(target_data['end'][i]),num)]
+            line2 = [(int(target_data['start'][i])-0.5,num), (int(target_data['end'][i])+0.5,num)]
             (line2_xs, line2_ys) = zip(*line2)
             ax.add_line(lines.Line2D(line2_xs, line2_ys, 
-                                fillstyle=None,linewidth=5, alpha = 1,color=color_dict[target_data['record'][i]]))
+                                solid_capstyle = 'butt',solid_joinstyle='miter',
+                                linewidth=4, alpha = 1,
+                                color=color_dict[target_data['record'][i]],
+                                antialiased=False))
         else:
             warnings.append(target_data['record'][i])
     if warnings:
         print('\nTips: ')
         print(' and '.join([i for i in set(warnings)]) + ' is not in our consideration!!!!!!')
-
-
+ 
+ 
     # 做图例
-    ax_legend = fig.add_axes([0.8,0.2,0.2,0.6])
+    # add_axes 是在一张图上指定特定区域作图，第一个数字为从左边%74处，下面20%处开始，宽20%，高60%区域作图
+    ax_legend = fig.add_axes([0.76,0.2,0.2,0.6])
     #ax_legend.set_xticks([])
     #ax_legend.set_yticks([])
-    for i in range(len(color_dict.keys())-1):
+    for i in range(len(colors_legend_name)):
         line3 = [(0, (9-i)*0.1),(0.1, (9-i)*0.1)]
         (line3_xs, line3_ys) = zip(*line3)
         ax_legend.add_line(lines.Line2D(line3_xs, line3_ys, linewidth=5,
-                                color=color_dict[names_tmp_colors[i]]))
+                                color=color_dict[names_tmp_colors[i]],
+                                solid_capstyle = 'butt',solid_joinstyle='miter',
+                                antialiased=False))
         ax_legend.text(0.2, (8.9-i)*0.1,colors_legend_name[i] , fontsize=6)
     ax_legend.set_axis_off()
     # 加标题
-    fig.suptitle('\n\n\nchr' + str(target_data['chr'][0])+ ': ' + gene_name, fontsize=10)
+    fig.suptitle('\n\n\nchr' + str(target_data['chr'][0])+ ': ' + gene_symbol, fontsize=10)
     # 保存图片
     fig.savefig(png_path, dpi=150)
     print('\nThe picture file is completed: ' + png_path)
     return(png_path)
-
-    
-
+ 
+     
+ 
 # gtf文件下载地址：
-#ftp://ftp.ensembl.org/pub/release-87/gtf/homo_sapiens/Homo_sapiens.GRCh38.87.chr.gtf.gz
+#[url]ftp://ftp.ensembl.org/pub/release-87/gtf/homo_sapiens/Homo_sapiens.GRCh38.87.chr.gtf.gz[/url]
 gtf_file_path = r'E:\r\biotrainee_demo\class5\Homo_sapiens.GRCh38.87.chr.gtf'
-
-gene_name = 'AnXA1'
-gene_name = 'hoxc8'
+import os
+os.chdir(r'E:\r\biotrainee_demo\class5')
 gene_name = 'TP53'
+gene_name = 'WASH7P'
+gene_name = 'hoxc8'
+gene_name = 'AnXA1'
+
+
 #计算消耗时间
 import time
 start = time.time()
@@ -150,7 +167,6 @@ start = time.time()
 # 提取目标基因的数据
 target_data = find_target_data(gene_name=gene_name, gtf_file_path=gtf_file_path)
 # 作图
-draw_gene_structure(gene_name,target_data, png_path=gene_name+'.png')
-
+draw_gene_structure(gene_name,target_data)
+ 
 print('\n used %.2f s to get the target information and get its structure!'%(time.time()-start))
-
